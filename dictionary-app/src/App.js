98594b163487897241f08b03c5ea6fcf3a299846
celenaal <101164218+celenaal@users.noreply.github.com>
2022-06-29 20:59:37 -0700

    react was added

---
+++ dictionary-app/src/App.js
@@ -0,0 +1,16 @@
+import Dictionary from "./Dictionary";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Celena Alvarado</footer>
+      </div>
+    </div>
+  );
+}
